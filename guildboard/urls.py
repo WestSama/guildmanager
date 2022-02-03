4b29b609f1360032160d340fe7cb7f83aef225ee
Manuel Fernandes <leetlordpt@gmail.com>
2022-02-03 15:55:02 +0000

    Redo the project

---
+++ guildboard/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+
+    path("login/", views.loginView, name="login"),
+    path("register/", views.registerView, name="register"),
+    path("logout/", views.logoutView, name="logout"),
+
+]
---
+++ guildboard/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib import messages
+
+from .forms import CreateUserForm
+
+def index(request):
+    return render(request, "guildboard/index.html")
+
+def loginView(request):
+
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            
+            return redirect("index")
+        else:
+            messages.info(request, "Username OR Password is incorrect")
+
+    context = {}
+    return render(request, "guildboard/login.html", context)
+
+def registerView(request):
+    form = UserCreationForm()
+    
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get("username")
+            messages.success(request, "Account was created for " + user)
+            
+            return redirect('login')
+
+    context = {"form":form}
+    return render(request, "guildboard/register.html", context)
+
+def logoutView(request):
+    logout(request)
+    return redirect("login")
