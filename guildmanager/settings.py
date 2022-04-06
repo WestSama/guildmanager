4423ab792162612f99c407a6189596e31ad4f488
Manuel Fernandes <93879306+WestSama@users.noreply.github.com>
2022-04-06 13:34:46 +0100

    Update settings.py

--- guildmanager/settings.py
+++ guildmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-37lrmia#euxhp=i1^fvr-jl(=wk7cz!b^+u$jce5)t66ymki+%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
